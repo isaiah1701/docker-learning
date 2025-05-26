899b9ede543a10c4f5e0071404f3a4f82ff8cd78
isaiah1701 <imichael1403@gmail.com>
2025-05-26 01:52:44 +0100

    adding docker counter/revision site v1

--- flask-challenge/app.py
+++ flask-challenge/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import redis
 import os
 
@@ -11,34 +11,13 @@ redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=Tr
 
 @app.route('/')
 def welcome():
-    return "Welcome to the Flask + Redis Challenge!"
+    visits = redis_client.incr('counter')
+    return render_template("index.html", count=visits)
 
 @app.route('/count')
 def count():
     visits = redis_client.incr('counter')
-    return f"Visit count: {visits} bro"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-from flask import Flask
-import redis
-import os
-
-app = Flask(__name__)
-
-# Read Redis connection details from env variables
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
-redis_port = int(os.environ.get('REDIS_PORT', 6379))
-redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
-
-@app.route('/')
-def welcome():
-    return "Welcome to the Flask + Redis Challenge!"
-
-@app.route('/count')
-def count():
-    visits = redis_client.incr('counter')
-    return f"Visit count: {visits}"
+    return render_template("count.html", count=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
